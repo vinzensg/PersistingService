f5fb5baee2bccbdb6f832967ceb6631e55a32c3e
Gianluca Vinzens <vinzensg@student.ethz.ch>
2012-04-16 16:42:00 +0200

    added static constants class

---
+++ src/config/Constants.java
@@ -0,0 +1,14 @@
+package config;
+
+public class Constants {
+
+	// Date storage
+	public final static String DATE_FORMAT = "yyyy/MM/dd HH:mm:ss";
+	
+	// Resource modes
+	public final static int PUSH_PUSH = 0; 
+	public final static  int POLLING_PUSH = 1;
+	public final static int PUSH_STORE = 2;
+	public final static int POLLING_STORE = 3;
+	
+}
